0824130c57aad14455abe2202fc25901d44c6ff6
Suami-Yonashiro <suami.yy@gmail.com>
2026-06-01 14:32:40 -0300

    Delete 01-projetos/01-quiz-claude-code/03-python-avancado-para-dados directory

--- 01-projetos/01-quiz-claude-code/03-python-avancado-para-dados/aula-01/exemplo_01.py
+++
@@ -1,6 +0,0 @@
-# Crie programa que o usuário digita o seu nome e retorna o número de caracteres.
-
-nome = input("Digite o seu nome: ")
-tamanho = len(nome)
-
-print(f"Seu nome tem: {tamanho} caracteres")
